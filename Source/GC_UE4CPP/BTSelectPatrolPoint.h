27e2e43b379a91e6cc2fe150b15adc9e84ae4d1a
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2022-01-24 16:17:06 +0100

    Better AI detection
    Using Observer on BT

--- Source/GC_UE4CPP/BTSelectPatrolPoint.h
+++ Source/GC_UE4CPP/BTSelectPatrolPoint.h
@@ -17,5 +17,4 @@ class GC_UE4CPP_API UBTSelectPatrolPoint : public UBTTaskNode
 		// EBTNodeResult -> like boolean but with other possibilities of return (like : succeeded, failed)
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
-
 };
